33bab2b3bcbc1f944847e2b9d9524c3dd1c7df35
Shozaib-Khan <70322458+Shozaib-Khan@users.noreply.github.com>
2023-11-10 23:42:12 +0530

    Update aptosgpt.py

--- aptosgpt.py
+++ aptosgpt.py
@@ -15,7 +15,6 @@ import constants
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-# Enable to save to disk & reuse the model (for repeated queries on the same data)
 PERSIST = False
 
 query = None
@@ -27,8 +26,7 @@ if PERSIST and os.path.exists("persist"):
   vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
   index = VectorStoreIndexWrapper(vectorstore=vectorstore)
 else:
-  loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  #loader = DirectoryLoader("data/")
+  loader = DirectoryLoader("data/")
   if PERSIST:
     index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
   else:
